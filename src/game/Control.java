52dfecee4d5cff8549234156e76e601b1a51743a
Noah <n_kess@outlook.de>
2021-10-09 12:46:42 +0200

    Some Update and Save before making dangerous changes

--- src/game/Control.java
+++ src/game/Control.java
@@ -2,6 +2,7 @@ package game;
 
 import gui.GUI;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Control {
@@ -165,6 +166,32 @@ public class Control {
         }
 
     }
+
+//    public void checkN(Cell[][] arr, Cell[][] futureGen, int columns, int rows) {
+//        ArrayList<Thread> threads = new ArrayList<>();
+//        final int NUM_OF_THREADS = 8; //Or can be passed as an argument
+//        for (int tid = 0; tid < NUM_OF_THREADS; tid++) {
+//            Integer thread_local_row_start = tid * rows / NUM_OF_THREADS;
+//            Integer thread_local_row_end = (tid + 1) * rows / NUM_OF_THREADS;
+//            Thread t = new Thread(() -> {
+//                for (int row = thread_local_row_start; row < thread_local_row_end; row++) {
+//                    for (int column = 0; column < columns; column++) {
+//                        countNeighbors(arr, row, column, rows, columns);
+//                        evaluateNeighbors(arr[row][column], futureGen[row][column]);
+//                    }
+//                }
+//            });
+//            t.start();
+//            threads.add(t);
+//        }
+//        for (Thread t : threads) {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     public boolean[][] nextGenBackup() {
 
